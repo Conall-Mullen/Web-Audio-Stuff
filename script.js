cfade21e1669861b6c7fe7fd1ce68df59ea04d15
Conall O Maolain <conallomaolain1@gmail.com>
2024-05-05 18:45:22 +0200

    add buttons dependant on number of samples loaded

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 console.clear();
 const audioContext = new AudioContext();
-
 const audioFiles = [
-  "assets/Guitar fill 3.wav",
-  "assets/Piano Chord.wav",
-  "assets/Piano Chord 2.wav",
-  "assets/What can I do .wav",
+  "samples/MR16_BD_02_T1S.wav",
+  "samples/MR16_Cabasa_X1_st.wav",
+  "samples/MR16_Clap_X1.wav",
+  "samples/MR16_CongaHigh_T1S.wav",
+  "samples/MR16_CongaLow_T1A.wav",
+  "samples/MR16_Cow_C2A.wav",
+  "samples/MR16_Cym_T1A.wav",
+  "samples/MR16_HH_C2A.wav",
+  "samples/MR16_HHo_T1A.wav",
+  "samples/MR16_Ride_T1A.wav",
+  "samples/MR16_Rim_T1A.wav",
+  "samples/MR16_Snr_T1S.wav",
+  "samples/MR16_WoodBlock_C2S.wav",
 ];
+let samples;
+const body = document.getElementsByTagName("body");
+for (let i = 0; i < audioFiles.length; i++) {
+  const newSamplePad = document.createElement("section");
+  newSamplePad.classList.add("button");
+  document.body.append(newSamplePad);
+}
 
-/*------------------------
-Load files to audio buffer
-------------------------*/
-
+const samplePads = Array.from(document.querySelectorAll(".button"));
+console.log(samplePads);
 async function loadAudioFile(url) {
   try {
     // Fetch audio file
@@ -28,28 +41,27 @@ async function setUpSamples(paths) {
   const audioBuffers = [];
   for (const path of paths) {
     const sample = await loadAudioFile(path);
+    sample.name = path;
     audioBuffers.push(sample);
   }
   console.log("Sample set up done");
+  console.log(audioBuffers);
   return audioBuffers;
 }
-function playSample(audioBuffer, time) {
-  const sampleSource = audioContext.createBufferSource();
-  sampleSource.buffer = decodedAudioData;
+
+function playAudio(buffer, time) {
+  const source = audioContext.createBufferSource();
+  source.buffer = buffer;
+  source.connect(audioContext.destination);
+  source.start(time);
 }
 
-/*---------------------------------
-Event listeners to trigger playback
----------------------------------*/
-
-// const audioElements = Array.from(document.querySelectorAll('[class="audio"]'));
-const button = document.querySelector('[class="button"]');
-const volumeFader = document.querySelector('[class="faders"]');
-
-button.addEventListener("click", () => {
-  console.log("Sample pad clicked");
+setUpSamples(audioFiles).then((response) => {
+  samples = response;
 });
 
-volumeFader.addEventListener("input", function (event) {
-  console.log(volumeFader.value);
-});
+for (let index = 0; index < samplePads.length; index++) {
+  samplePads[index].addEventListener("click", () => {
+    playAudio(samples[index], 0);
+  });
+}
