b1e20a1501c5f1ef3006c88a4916dab43bd79a69
Conall O Maolain <conallomaolain1@gmail.com>
2024-05-05 22:36:09 +0200

    change variable names in play function

--- script.js
+++ script.js
@@ -3,20 +3,18 @@ const audioContext = new AudioContext();
 const audioFiles = [
   "samples/MR16_BD_02_T1S.wav",
   "samples/MR16_Snr_T1S.wav",
-  "samples/MR16_Cabasa_X1_st.wav",
-  "samples/MR16_Clap_X1.wav",
-  "samples/MR16_CongaHigh_T1S.wav",
-  "samples/MR16_CongaLow_T1A.wav",
-  "samples/MR16_Cow_C2A.wav",
-  "samples/MR16_Cym_T1A.wav",
-  "samples/MR16_HH_C2A.wav",
-  "samples/MR16_HHo_T1A.wav",
-  "samples/MR16_Ride_T1A.wav",
-  "samples/MR16_Rim_T1A.wav",
-  "samples/MR16_WoodBlock_C2S.wav",
-  "assets/Guitar fill 3.wav",
-  "assets/Piano Chord.wav",
-  "assets/Piano Chord 2.wav",
+  // "samples/MR16_Cabasa_X1_st.wav",
+  // "samples/MR16_Clap_X1.wav",
+  // "samples/MR16_CongaHigh_T1S.wav",
+  // "samples/MR16_CongaLow_T1A.wav",
+  // "samples/MR16_Cow_C2A.wav",
+  // "samples/MR16_Cym_T1A.wav",
+  // "samples/MR16_HH_C2A.wav",
+  // "samples/MR16_HHo_T1A.wav",
+  // "samples/MR16_Ride_T1A.wav",
+  // "samples/MR16_Rim_T1A.wav",
+  // "samples/MR16_WoodBlock_C2S.wav",
+  // "assets/Guitar fill 3.wav",
 ];
 let samples;
 const body = document.getElementsByTagName("body");
@@ -58,10 +56,12 @@ async function setUpSamples(paths) {
 }
 
 function playAudio(buffer, time) {
-  const source = audioContext.createBufferSource();
-  source.buffer = buffer;
-  source.connect(audioContext.destination);
-  source.start(time);
+  const bufferSourceNode = audioContext.createBufferSource();
+  const gainNode = audioContext.createGain();
+  bufferSourceNode.buffer = buffer;
+  bufferSourceNode.connect(gainNode);
+  gainNode.connect(audioContext.destination);
+  bufferSourceNode.start(time);
 }
 
 setUpSamples(audioFiles).then((response) => {
@@ -72,7 +72,8 @@ for (let index = 0; index < audioFiles.length; index++) {
   samplePads[index].addEventListener("click", () => {
     playAudio(samples[index], 0);
   });
-  gainFaders[index].addEventListener("input", () => {
-    console.log((100 - gainFaders[index].value) / 100);
+  gainFaders[index].addEventListener("input", (event) => {
+    let sliderValue = (100 - event.target.value) / 100;
+    console.log(sliderValue);
   });
 }
